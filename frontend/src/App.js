07a9cfbce3a6ae1bda271b65c15ce78c91e65555
ramosplanilhas-ops <ramos.planilhas@gmail.com>
2025-08-26 00:56:05 +0000

    Adiciona projeto React

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/images/logo.svg" className="App-logo" alt="logo" />
+        <p>
+          Olá, Mundo! Meu primeiro app React no Gitpod!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
